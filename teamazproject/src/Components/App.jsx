ac80277ecf0e2e5f6c99d50d61675a2f35585b8a
Zachary <taylorzw96@gmail.com>
2021-07-23 11:02:53 -0500

    Added Embedded Video Player with generic videos from Youtube

--- teamazproject/src/Components/App.jsx
+++ teamazproject/src/Components/App.jsx
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
+import Video from './VideoPlayer/VideoPlayer';
 
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {  
+            videos: []
+        }
     }
+
     render() { 
         return (  
             <React.Fragment>
-                
+                <Video />
             </React.Fragment>
         );
     }
