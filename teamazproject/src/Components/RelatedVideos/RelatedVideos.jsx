24dbca9557bba7a7f1e9c02571118d52a338cc83
Andy Wade <carrollandywadejr@gmail.com>
2021-07-27 12:40:19 -0500

    added thumbnails for 5 related videos

---
+++ teamazproject/src/Components/RelatedVideos/RelatedVideos.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const RelatedVideos = (props) => {
+    return ( 
+        <div>
+            <img src={this.state.thumbnail_video_0} />
+            <img src={this.state.thumbnail_video_1} />
+            <img src={this.state.thumbnail_video_2} />
+            <img src={this.state.thumbnail_video_3} />
+            <img src={this.state.thumbnail_video_4} />
+        </div>
+     );
+}
+ 
+export default RelatedVideos;
