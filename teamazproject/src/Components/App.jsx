5752f73acf8b38f4482b367fa79357f7cd97bb27
Andy Wade <carrollandywadejr@gmail.com>
2021-07-27 11:58:51 -0500

    added thumbnails to the player that displays 5 related videos

--- teamazproject/src/Components/App.jsx
+++ teamazproject/src/Components/App.jsx
@@ -9,32 +9,74 @@ class App extends Component {
         this.state = {  
             videos: [],
             search_input: '',
-            selected_video: ''
+            selected_video: '',
+            related_video_0: '',
+            related_video_1: '',
+            related_video_2: '',
+            related_video_3: '',
+            related_video_4: '',
+            thumbnail_video_0: '',
+            thumbnail_video_1: '',
+            thumbnail_video_2: '',
+            thumbnail_video_3: '',
+            thumbnail_video_4: '',
         }
     }
     componentDidMount() {
         this.defaultVideo()
     }
     defaultVideo = async () => {
-        let result = await axios.get(`https://www.googleapis.com/youtube/v3/search/?q=BkD2nN5275c$&type=video&videoDuration=any&maxResults=5&key=AIzaSyC7Nfr75k-uhIlTRn5H1XHWvP0Fl9zM8Pg`);
+        let result = await axios.get(`https://www.googleapis.com/youtube/v3/search/?q=BkD2nN5275c$&type=video&videoDuration=any&maxResults=5&key=AIzaSyDPRvkeCRQj03kouGwoU9Vfs84NQDnOmUw`);
         this.setState({
             videos: result.data,
             selected_video: result.data.items[0].id.videoId,
+            // related_video_1: result.data.items[1].id.videoId,
         });
         console.log(this.state.selected_video)
+        this.relatedVideos()
+        // console.log(this.state.related_video_1)
         // this.displayVideos()
     }
 
     searchVideos = async (search) => {
         console.log(search)
         console.log(this.state.search_input)
-        let result = await axios.get(`https://www.googleapis.com/youtube/v3/search/?q=${search}&type=video&videoDuration=any&maxResults=5&key=AIzaSyC7Nfr75k-uhIlTRn5H1XHWvP0Fl9zM8Pg`);
+        let result = await axios.get(`https://www.googleapis.com/youtube/v3/search/?q=${search}&type=video&videoDuration=any&maxResults=5&key=AIzaSyDPRvkeCRQj03kouGwoU9Vfs84NQDnOmUw`);
         this.setState({
             videos: result.data,
             selected_video: result.data.items[0].id.videoId,
             // search_input: this.state.search_input
         });
         console.log(this.state.selected_video)
+        this.relatedVideos()
+    }
+
+    relatedVideos = async () => {
+        let result = await axios.get(`https://www.googleapis.com/youtube/v3/search/?relatedToVideoId=${this.state.selected_video}&type=video&videoDuration=any&maxResults=5&key=AIzaSyDPRvkeCRQj03kouGwoU9Vfs84NQDnOmUw&part=snippet`);
+        this.setState({
+            related_video_1: result.data.items[0].id.videoId,
+            related_video_1: result.data.items[1].id.videoId,
+            related_video_2: result.data.items[2].id.videoId,
+            related_video_3: result.data.items[3].id.videoId,
+            related_video_4: result.data.items[4].id.videoId,
+            thumbnail_video_0: result.data.items[0].snippet.thumbnails.default.url,
+            thumbnail_video_1: result.data.items[1].snippet.thumbnails.default.url,
+            thumbnail_video_2: result.data.items[2].snippet.thumbnails.default.url,
+            thumbnail_video_3: result.data.items[3].snippet.thumbnails.default.url,
+            thumbnail_video_4: result.data.items[4].snippet.thumbnails.default.url,
+
+        })
+        console.log(this.state.related_video_0)
+        console.log(this.state.related_video_1)
+        console.log(this.state.related_video_2)
+        console.log(this.state.related_video_3)
+        console.log(this.state.related_video_4)
+        console.log(this.state.selected_video)
+        console.log(this.state.thumbnail_video_0)
+        console.log(this.state.thumbnail_video_1)
+        console.log(this.state.thumbnail_video_2)
+        console.log(this.state.thumbnail_video_3)
+        console.log(this.state.thumbnail_video_4)
     }
 
     render() { 
@@ -43,6 +85,13 @@ class App extends Component {
                 <h1>Youtube Clone</h1>
                 <VideoSearch videos={this.state.videos} searchVideos={this.searchVideos}/>
                 <VideoPlayer default_video={this.state.selected_video}/>
+                <div>
+                    <img src={this.state.thumbnail_video_0} />
+                    <img src={this.state.thumbnail_video_1} />
+                    <img src={this.state.thumbnail_video_2} />
+                    <img src={this.state.thumbnail_video_3} />
+                    <img src={this.state.thumbnail_video_4} />
+                </div>
             </React.Fragment>
         );
     }
