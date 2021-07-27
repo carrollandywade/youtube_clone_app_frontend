3b4e828c6600c68c2b1280a2efbd1db78aa28b8f
Andy Wade <carrollandywadejr@gmail.com>
2021-07-27 08:28:20 -0500

    refactored for react best practices, also added a default video with the ability for the user to choose a new video

--- teamazproject/src/Components/App.jsx
+++ teamazproject/src/Components/App.jsx
@@ -1,26 +1,48 @@
 import React, {Component} from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import VideoSearch from './VideoSearch/videoSearch';
-// import VideoPlayer from './VideoPlayer/VideoPlayer';
+import VideoPlayer from './VideoPlayer/VideoPlayer';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {  
-            videos: [
-                {videoId: ''},
-                {thumbnails: ''}
-            ]
+            videos: [],
+            search_input: '',
+            selected_video: ''
         }
     }
+    componentDidMount() {
+        this.defaultVideo()
+    }
+    defaultVideo = async () => {
+        let result = await axios.get(`https://www.googleapis.com/youtube/v3/search/?q=BkD2nN5275c$&type=video&videoDuration=any&maxResults=5&key=AIzaSyC7Nfr75k-uhIlTRn5H1XHWvP0Fl9zM8Pg`);
+        this.setState({
+            videos: result.data,
+            selected_video: result.data.items[0].id.videoId,
+        });
+        console.log(this.state.selected_video)
+        // this.displayVideos()
+    }
 
+    searchVideos = async (search) => {
+        console.log(search)
+        console.log(this.state.search_input)
+        let result = await axios.get(`https://www.googleapis.com/youtube/v3/search/?q=${search}&type=video&videoDuration=any&maxResults=5&key=AIzaSyC7Nfr75k-uhIlTRn5H1XHWvP0Fl9zM8Pg`);
+        this.setState({
+            videos: result.data,
+            selected_video: result.data.items[0].id.videoId,
+            // search_input: this.state.search_input
+        });
+        console.log(this.state.selected_video)
+    }
 
     render() { 
         return (  
             <React.Fragment>
                 <h1>Youtube Clone</h1>
-                <VideoSearch videos={this.state.videos} />
-                {/* <VideoPlayer playVideos={this.displayVideos} /> */}
+                <VideoSearch videos={this.state.videos} searchVideos={this.searchVideos}/>
+                <VideoPlayer default_video={this.state.selected_video}/>
             </React.Fragment>
         );
     }
--- teamazproject/src/Components/VideoSearch/videoSearch.jsx
+++ teamazproject/src/Components/VideoSearch/videoSearch.jsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 import React, {Component} from 'react';
 
 // API_KEY: AIzaSyCwMBHagNKMBnjxT_hCxAC1yGzU2M-W6II
@@ -26,37 +26,39 @@ class VideoSearch extends Component {
 
     onSubmitHandler = (event) => {
         event.preventDefault();
-        this.searchVideos(this.state.search_input);
+        const search = (this.state.search_input)
+        // this.searchVideos(this.state.search_input);
+        this.props.searchVideos(search)
     }
 
-    searchVideos = async () => {
-        let result = await axios.get(`https://www.googleapis.com/youtube/v3/search/?q=${this.state.search_input}&type=video&videoDuration=any&maxResults=5&key=AIzaSyBTe5Zf-dRotWKLeNJDGIpyzHPatfCGRIc`);
-        this.setState({
-            videos: result.data,
-            selected_video: result.data.items[0].id.videoId,
-            search_input: this.state.search_input
-        });
-        console.log(this.state.videos)
-        this.displayVideos()
-    }
+    // searchVideos = async () => {
+    //     let result = await axios.get(`https://www.googleapis.com/youtube/v3/search/?q=${this.state.search_input}&type=video&videoDuration=any&maxResults=5&key=AIzaSyCwMBHagNKMBnjxT_hCxAC1yGzU2M-W6II`);
+    //     this.setState({
+    //         videos: result.data,
+    //         selected_video: result.data.items[0].id.videoId,
+    //         search_input: this.state.search_input
+    //     });
+    //     // console.log(this.state.videos)
+    //     // this.displayVideos()
+    // }
 
     
-    displayVideos = async (event) => {
-        console.log("State - Videos: ",this.state.videos)
-        console.log("State - video: ",this.state.videos.items[0].id.videoId)
-        // let res = await axios.get(`https://www.youtube.com/embed/${this.state.videos.items[0].id.videoId}?autoplay=1&origin=http://example.com`)
-        // console.log("Player Response: ", res)
-        // console.log(res.data.items.id.videoId)
-        // this.setState({
-        //     thumbnails: res.data
-        //    });
-        // this.state.videos.map((video, videoId) => {
-        //    return(
-        //     //    key = video_id
-        //        this.state.video.thumbnails
-        //    )
-        // });
-    }
+    // displayVideos = async (event) => {
+    //     console.log("State - Videos: ",this.state.videos)
+    //     console.log("State - video: ",this.state.videos.items[0].id.videoId)
+    //     // let res = await axios.get(`https://www.youtube.com/embed/${this.state.videos.items[0].id.videoId}?autoplay=1&origin=http://example.com`)
+    //     // console.log("Player Response: ", res)
+    //     // console.log(res.data.items.id.videoId)
+    //     // this.setState({
+    //     //     thumbnails: res.data
+    //     //    });
+    //     // this.state.videos.map((video, videoId) => {
+    //     //    return(
+    //     //     //    key = video_id
+    //     //        this.state.video.thumbnails
+    //     //    )
+    //     // });
+    // }
 
     render() {
         return (
@@ -66,7 +68,7 @@ class VideoSearch extends Component {
                         <input type="text" name="search_input"  onChange={this.onChangeHandler} value={this.state.search_input} placeholder="Search for video here." />
                         <button type="submit" className="btn btn-primary w-md-25">Search</button>
                     </form>
-                    <iframe src={`https://www.youtube.com/embed/${this.state.selected_video}?autoplay=1&origin=http://example.com`}></iframe>
+                    {/* <iframe src={`https://www.youtube.com/embed/${this.state.selected_video}?autoplay=1&origin=http://example.com`}></iframe> */}
                 </div>
             </React.Fragment>
         )
